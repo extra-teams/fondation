b68674bcb1323d880e1030c3c7d663e2ae87120d
kevinG73 <kevinguelade@gmail.com>
2021-06-19 15:06:49 +0000

    UPD section team

--- resources/views/layouts/partial/footer.blade.php
+++ resources/views/layouts/partial/footer.blade.php
@@ -1,40 +1,15 @@
-<div class="container-fluid footer">
+<div class="container">
+    <hr>
     <div class="row">
-        <div class="col-lg-3 col-12">
-            <h6 class="text-uppercase">Contacts</h6>
-            <ul class="list-unstyled">
-                <li><a href="{{url('/contact')}}">contacts</a></li>
-                <li><a href="{{route('politiqueretour')}}">politique de retour</a></li>
+        <div class="col-12 col-md-6">
+            <span>@2021 - Fondation 225</span>
+        </div>
+        <div class="col-12 col-md-6">
+            <ul class="d-flex justify-content-end list-unstyled">
+                <li class="pr-2"><a href="#">Politique de confidentialité</a></li>
+                <li class="pr-2"><a href="#">Mentions légales</a></li>
+                <li class="pr-2"><a href="#">Crédits</a></li>
             </ul>
         </div>
-
-        <div class="col-lg-3 col-12">
-            <h6 class="text-uppercase">A PROPOS</h6>
-            <ul class="list-unstyled">
-                <li><a href="{{route('apropos')}}">qui sommes-nous ?</a></li>
-                <li><a href="{{route('confidentialité')}}">politique de confidentialité</a></li>
-                <li><a href="{{route('conditions')}}">conditions générales d'utilisation</a></li>
-            </ul>
-        </div>
-
-
-        <div class="col-lg-3 col-12">
-            <h6 class="text-uppercase">mon compte</h6>
-            <ul class="list-unstyled">
-                <li><a href="{{url('/info')}}">mes informations</a></li>
-                <li><a href="{{url('/mes-commandes')}}">mes commandes</a></li>
-            </ul>
-        </div>
-
-        <div class="col-lg-3 col-12">
-            <h6 class="text-uppercase">réseaux sociaux</h6>
-            @php $liens= DB::table('settings')->get(); @endphp
-            <ul class="list-unstyled">
-                <li><a href="{{$liens[0]->value}}">facebook</a></li>
-                <li><a href="{{$liens[1]->value}}">twitter</a></li>
-                <li><a href="{{$liens[2]->value}}">instagram</a></li>
-            </ul>
-        </div>
-
     </div>
 </div>

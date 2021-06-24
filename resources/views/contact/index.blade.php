e88c3157be87ca88a01246d2d783e4b6763b1921
kevinG73 <kevinguelade@gmail.com>
2021-06-24 12:57:50 +0000

    UPD contact page

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,56 +1,25 @@
 @extends('layouts.app')
-@section('title','contactez-nous Fondation225')
+@section('title','contactez-nous - Fondation225')
 @section('content')
-<style>
-    .font-roboto-condensed {
-    font-family: "Roboto Condensed", sans-serif;
-}
-.font-weight-bold {
-    font-weight: 700 !important;
-}
-h2, .h2 {
-    font-size: 2rem;
-}
-
-h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
-    margin-bottom: 0.5rem;
-    font-weight: 500;
-    line-height: 1.2;
-}
-</style>
-<!-- content -->
+    <!-- content -->
     <!-- breadcrumb -->
-    <div class="container-fluid p-0">
-        <div class="article bg-callcentre" style="  background: url(images/banner/pain.jpg);
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-position: center center;
-        height: 15rem;">
+    <div class="container-fluid p-0" id="contact">
+        <div class="callcentre">
             <!-- breadcrumb -->
-            <div class="article__header p-5">
+            <div class="header">
                 <div class="row">
                     <div class="col-12">
-                        <div class="pt-3">
-                            <h2  class="font-roboto-condensed font-weight-bold styled_title ">Nos contacts</h2>
-                            {{-- <span style="border-bottom: solid black; width:50px;">&nbsp;&nbsp;</span> --}}
-                        {{-- <hr style="height: 3px; color: black; font-weigth: bold;"> --}}
-                        </div>
+                        <h2 class="styled_title text-white">Nos contacts</h2>
                     </div>
                 </div>
             </div>
-            <!-- endbreadcrumb -->
-
         </div>
-    </div>
 
-    <!-- agences -->
-    <div class="container-fluid mt-5 mb-5">
         <!-- première ligne -->
         <div class="row mb-5">
-
             <div class="col-12 col-lg-6 align-self-center mt-5 mt-lg-0">
                 <div class="pl-3 text-left">
-                    <h3 class="font-weight-bold text-uppercase styled_title">
+                    <h3 class="styled_title">
                         En ligne
                     </h3>
                     <p class="font-roboto-condensed">
@@ -58,41 +27,39 @@ h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
                         directement en prenant contact avec un conseiller en nous envoyant un mail
                         à l'adresse mail suivante :
                     </p>
-                    <a href="mailto:contact@bms.ci" class="btn btn-black" style="background-color: black; color: white;">
+                    <a href="mailto:contact@bms.ci" class="btn bg-orange">
                         <i class="fa fa-share"></i>
                         contact@fondation225.ci
                     </a>
-
                 </div>
             </div>
 
             <div class="col-12 col-lg-6 align-self-center mt-5 mt-lg-0">
                 <div class="pl-3 text-left">
-                    <h3 class="font-weight-bold text-uppercase styled_title">
+                    <h3 class="styled_title">
                         Réseaux sociaux
                     </h3>
                     <p class="font-roboto-condensed">
                         Posez vos questions via un de nos réseaux sociaux ci-dessous.
                     </p>
-                    <a href="" class="btn btn-facebook" style="color: white;background-color: #3b5998;">
-                        <i class="fa fa-facebook"></i>
+                    <a href="#" class="btn bg-facebook text-white">
+                        <i class="fab fa-facebook"></i>
                         FACEBOOK
                     </a>
 
-                    <a href="" class="btn btn-twitter" style="color: white;background-color: #1DA1F2;">
-                        <i class="fa fa-twitter"></i>
+                    <a href="#" class="btn bg-twitter text-white">
+                        <i class="fab fa-twitter"></i>
                         TWITTER
                     </a>
                 </div>
             </div>
-
         </div>
+
         <!-- deuxième ligne -->
         <div class="row">
-
             <div class="col-12 col-lg-6 align-self-center mt-5 mt-lg-0">
                 <div class="pl-3 text-left">
-                    <h3 class="font-weight-bold text-uppercase styled_title">
+                    <h3 class="styled_title">
                         <i class="fa fa-phone"></i>
                         Par téléphone
                     </h3>
@@ -100,7 +67,7 @@ h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
                         Nous restons joignable les jours ouvrables de 08h00 à 15h00.
                     </p>
 
-                    <a href="#" class="btn btn-black" style="background-color: black; color: white;">
+                    <a href="#" class="btn bg-orange">
                         (+226) 51 54 12 12
                     </a>
                 </div>
@@ -108,17 +75,16 @@ h1, h2, h3, h4, h5, h6, .h1, .h2, .h3, .h4, .h5, .h6 {
 
             <div class="col-12 col-lg-6 align-self-center mt-5 mt-lg-0">
                 <div class="pl-3 text-left">
-                    <h3 class="font-weight-bold text-uppercase styled_title">
+                    <h3 class="styled_title">
                         Bureaux Fondation 225
                     </h3>
                     <p class="font-roboto-condensed">
-                        Du lundi au vendredi de 08h à 15h30, le samedi de 09h à 12h uniquement dans nos locaux à <strong>Ouagadougou (Burkina Faso)</strong>
+                        Du lundi au vendredi de 08h à 15h30, <br> Le samedi de 09h à 12h uniquement dans nos locaux à
+                        <strong>Ouagadougou (Burkina Faso)</strong>
                     </p>
                 </div>
             </div>
 
         </div>
     </div>
-    <!--  -->
-
 @endsection

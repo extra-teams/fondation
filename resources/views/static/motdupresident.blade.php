6fad3adf04a6291f940f0d6cd11a4ae45083b8c5
kevinG73 <kevinguelade@gmail.com>
2021-07-25 08:10:01 +0000

    Mise a jour de la page mot du president

--- resources/views/static/motdupresident.blade.php
+++ resources/views/static/motdupresident.blade.php
@@ -10,12 +10,14 @@
         </div>
     </div>
     <!-- texte -->
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-12">
-                <img style="height: 12rem" src="{{asset("images/equipe/president.jpg")}}" alt="">
-                <p class="main-introduction">
-                    La question du développement de l’Afrique se pose plus que jamais avec acuité au moment
+    <div class="container mt-5" id="mot-du-president">
+        <div class="row justify-content-center">
+            <div class="col-10 col-lg-4 img">
+                <img src="{{asset("images/equipe/president.jpg")}}" alt="">
+            </div>
+            <div class="col-11 col-lg-8">
+                <p class="text-justify">
+                    “ La question du développement de l’Afrique se pose plus que jamais avec acuité au moment
                     où l’on entame la deuxième décennie du troisième millénaire. À ce stade de la marche <br>
 
                     historique de l’humanité, notre continent continue comme par le passé de pointer à la
@@ -68,7 +70,7 @@
                     puissamment nos pays et boosteraient notablement leur progression sur l’échelle du
                     développement.<br>
 
-                    C’est pourquoi, la Fondation 225 a fait de l’une de ses missions principales la promotion des
+                    C’est pourquoi, <b>la Fondation 225</b> a fait de l’une de ses missions principales la promotion des
                     œuvres de ces femmes et de ces hommes comme des modèles d’action de développement.
                     Pour que chaque Africain se les approprie et que l’émulation ainsi créée, multiplie ces
                     initiatives à l’échelle de chaque pays et, in extenso, de l’ensemble du continent africain.
@@ -76,18 +78,17 @@
                     la Fondation 225 s’est allié au Groupe Safam Com International basé à Abidjan,le ciiectif des
                     journalistes pour la paix et le maintien de la democratie et d’autres structures poursuivant le
                     même but d’une Afrique qui s’envole vers le développement par l’implication première des
-                    Africains eux-mêmes, pour créer le PRIX AFRICAIN DE DEVELOPPEMENT ( PADEV).<br>
+                    Africains eux-mêmes, pour créer le <b>PRIX AFRICAIN DE DEVELOPPEMENT</b> ( PADEV). <br>
 
 
-                    C’est à cette distinction, créée en 2006, et dont la première édition a eu lieu en 2007 à
-                    l’Hôtel du 6-Février à Lomé au Togo, que nous consacrons ce site Internet.
+                    C’est à cette distinction, créée en <b>2006</b>, et dont la première édition a eu lieu en <b>2007</b> à
+                    l’Hôtel du 6-Février à <b>Lomé</b> au <b>Togo</b>, que nous consacrons ce site Internet.
                     Que les œuvres de ces femmes et de ces hommes, par leur qualité, permettent à leurs pays
                     respectifs de faire un pas significatif en avant sur la voie du développement et en inspirent
                     surtout de très nombreuses autres ! <br>
-                    Bien à vous, sœurs et frères Africains. <br>
-                    KOFFI KOUADIO/Mr <br>
-                    Président exécutif de la <br>
-                    Fondation 225 <br>
+                    Bien à vous, sœurs et frères Africains. “ <br>
+                    <h5>KOFFI KOUADIO/Mr</h5>
+                    <h5>Président exécutif de la Fondation 225</h5> <br>
 
                 </p>
             </div>
